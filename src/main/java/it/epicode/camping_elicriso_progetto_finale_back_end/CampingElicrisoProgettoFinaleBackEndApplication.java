f5f13870eabf9057b6ff8e4a6daeb3fb39be11ce
Vincenza <vinni2690@hotmail.com>
2025-07-01 11:28:48 +0200

    Initial commit

---
+++ src/main/java/it/epicode/camping_elicriso_progetto_finale_back_end/CampingElicrisoProgettoFinaleBackEndApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.camping_elicriso_progetto_finale_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingElicrisoProgettoFinaleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingElicrisoProgettoFinaleBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/camping_elicriso_progetto_finale_back_end/CampingElicrisoProgettoFinaleBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.camping_elicriso_progetto_finale_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingElicrisoProgettoFinaleBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
